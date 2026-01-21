d147f5c5b49922f9b31e0796094afa3fb083cfd6
mohammad53eu <mohammadqassas114@gmail.com>
2026-01-21 14:36:28 +0300

    adding cleanup thread functions & running signals

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "kvstore.hpp"
 #include "server.hpp"
 #include "persistence.hpp"
+#include <csignal>
+
+std::atomic<bool> running(true);
+
+void handle_signal(int) {
+    running = false;
+}
 
 int main() {
     KVStore store;
@@ -8,8 +15,15 @@ int main() {
     TCPServer server(8000, store, file);
     
     file.replay(store);
+    
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
 
-    server.start();
+    store.start_cleanup_thread();
+    server.start(running);
+    store.stop_cleanup_thread();
+
+
 
     
     return 0;
