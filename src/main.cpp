26d16b7c2fdb1b4e749797ff5e93d1967316aa03
mohammad53eu <mohammadqassas114@gmail.com>
2026-01-23 02:22:59 +0300

    addind args in int main() & NodeRole logic

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "server.hpp"
 #include "persistence.hpp"
 #include <csignal>
+#include <node_role.hpp>
 
 std::atomic<bool> running(true);
 
@@ -9,20 +10,31 @@ void handle_signal(int) {
     running = false;
 }
 
-int main() {
+NodeRole role = NodeRole::Leader;
+
+int main(int argc, char* argv[]) {
+    if (argc >= 2 && std::string(argv[1]) == "--follower") {
+    role = NodeRole::Follower;
+    }
     KVStore store;
-    PersistenceManager file("data.aof");
-    TCPServer server(8000, store, file);
+    PersistenceManager file(store, "data.aof");
+    TCPServer server(8000, store, file, role);
     
     file.replay(store);
+
+
+
+
     
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
 
+    
+    file.start_save_state_thread();
     store.start_cleanup_thread();
     server.start(running);
     store.stop_cleanup_thread();
-
+    file.stop_save_state_thread();
 
 
     
